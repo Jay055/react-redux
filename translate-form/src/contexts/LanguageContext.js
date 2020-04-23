719783459cfc3183737d3f985d3f0983e02a045b
jay055 <nwachukwuuche@gmail.com>
2020-04-23 22:09:53 +0200

    Completed Translate Form

--- translate-form/src/contexts/LanguageContext.js
+++ translate-form/src/contexts/LanguageContext.js
@@ -3,4 +3,25 @@ import React from "react";
 // We can include a default value in the context English then connect to our nexted children
 
 // console.log(React.createContext('english'));
-export default React.createContext("english");
+const Context = React.createContext("english");
+
+export class LanguageStore extends React.Component {
+  state = { language: "english" };
+
+ onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+// Create a Provider to pass the values we would need in other components 
+      <Context.Provider
+        value={{ ...this.state, onLanguageChange: this.onLanguageChange }} >
+          {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+
+export default Context;
